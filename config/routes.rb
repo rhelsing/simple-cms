466fbc209437b7481d716c57134826dbd5f5d9e8
Ryan Helsing <ryanhelsing@gmail.com>
2014-08-18 11:28:19 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'demo/index'
+  match ':controller(/:action(/:id(.:format)))', :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
