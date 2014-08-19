7e44f0d84a822b28c1e0e44682db074b91de9c29
Ryan Helsing <ryanhelsing@gmail.com>
2014-08-19 13:01:11 -0400

    Named scoped are cool

---
+++ app/models/admin_user.rb
@@ -0,0 +1,5 @@
+class AdminUser < ActiveRecord::Base
+	#self.table_name = "admin_users"#configuration because against convention if user
+
+	#adds code for each column in table
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ActiveRecord::Base
+	#very useful
+	scope :visible, lambda{where(:visible => true)}#Subject.visible
+	scope :invisible, lambda{where(:visible => false)}#Subject.invisible
+	scope :sorted,  lambda {order("subjects.position ASC")}
+	scope :newest_first,  lambda {order("subjects.created_at DESC")}
+	scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
+	#can chain scopes Subject.recent.visible.active
 end
